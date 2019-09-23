8b0ccc7c87dfe1bca9d24f6c07b784946af38f9b
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-09-23 15:03:51 +0300

    Handling emitted errors with the error event listener of Request obj

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const https = require('https');
 
-const getProfile = (username) => https.get(`https://teamtreehouse.com/${username}.json`, res => {
-    let body = "";
+const getProfile = (username) => { const req = https.get(`https://teamtreehouse.com/${username}.json`, res => {
+        let body = "";
 
-    //Functions
-    const printMessage = (name, badgesNumb, jsPoints) => {
-        console.log(`${name} has total ${badgesNumb} badge(s) and ${jsPoints} point(s) in JavaScript.`);
-    }
+        //Functions
+        const printMessage = (name, badgesNumb, jsPoints) => {
+            console.log(`${name} has total ${badgesNumb} badge(s) and ${jsPoints} point(s) in JavaScript.`);
+        };
 
-    res.on('data', data => {
-        body += data;
+        res.on('data', data => {
+            body += data;
+        });
+
+        res.on('end', ()=>{
+            let profile = JSON.parse(body);
+            const {name, badges, points} = profile;
+            printMessage(name, badges.length, points.JavaScript);                
+        });
     });
 
-    res.on('end', ()=>{
-        let profile = JSON.parse(body);
-        const {name, badges, points} = profile;
-        printMessage(name, badges.length, points.JavaScript);                
-    })
-});
-
+    //Handling error with the Request / Emitted errors (e.g false url)
+    req.on('error', error=>{
+        console.error(`Error with the request: ${error.message}`);
+    });
+}
 //Set users as arguments and put to an array
 const users = process.argv.slice(2);
 
