3bca9b1db6b9a6a7f0ca1f63db579651217b3cde
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-09-23 16:08:09 +0300

    Add trycatch block to handle url specification exceptions

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 const https = require('https');
 
-const getProfile = (username) => { const req = https.get(`https://teamtreehouse.com/${username}.json`, res => {
-        let body = "";
 
-        //Functions
-        const printMessage = (name, badgesNumb, jsPoints) => {
-            console.log(`${name} has total ${badgesNumb} badge(s) and ${jsPoints} point(s) in JavaScript.`);
-        };
+//const getProfile = (username)=>{
+function getProfile(username){ 
+    //Handling exceptions (e.g invalid urls - does not fit node specification)
+    try {
+        const req = https.get(`https://teamtreehouse.com/${username}.json`, res => {
+            let body = "";
 
-        res.on('data', data => {
-            body += data;
+            //Functions
+            const printMessage = (name, badgesNumb, jsPoints) => {
+                console.log(`${name} has total ${badgesNumb} badge(s) and ${jsPoints} point(s) in JavaScript.`);
+            };
+
+            res.on('data', data => {
+                body += data;
+            });
+
+            res.on('end', ()=>{
+                let profile = JSON.parse(body);
+                const {name, badges, points} = profile;
+                printMessage(name, badges.length, points.JavaScript);                
+            });
         });
 
-        res.on('end', ()=>{
-            let profile = JSON.parse(body);
-            const {name, badges, points} = profile;
-            printMessage(name, badges.length, points.JavaScript);                
+        //Handling error with the Request / Emitted errors (e.g false url)
+        req.on('error', error=>{
+            console.error(`Error with the request: ${error.message}`);
         });
-    });
-
-    //Handling error with the Request / Emitted errors (e.g false url)
-    req.on('error', error=>{
-        console.error(`Error with the request: ${error.message}`);
-    });
+    } catch (error) {
+        console.error(error.message);
+    }
 }
-//Set users as arguments and put to an array
-const users = process.argv.slice(2);
 
-users.forEach(getProfile);
+ //Set users as arguments and put to an array
+ const users = process.argv.slice(2);
+
+ users.forEach(getProfile);
+
