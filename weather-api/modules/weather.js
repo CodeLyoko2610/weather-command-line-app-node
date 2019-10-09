6d3f3f90c518fa655fb4325c98560a920d455a3a
CodeLyoko2610 <dangchuong1999@outlook.com>
2019-10-09 12:48:27 +0300

    review weather-api

--- weather-api/modules/weather.js
+++ weather-api/modules/weather.js
@@ -1,50 +1,49 @@
 const https = require('https');
 const http = require('http');
+const keys = require('../api.json');
 const querystring = require('querystring');
 
-//import modules
-const keys = require('../api');
+//Require modules
 const printMess = require('./printMess');
 const printError = require('./printError');
 
+const get = (query) => {
+    const parameters = {
+        appid: keys.APIKEY,
+        units: 'metrics',
+    }
+    
+    //Handle param
+    let zipCode = parseInt(query);
+    if(!isNaN(zipCode)){
+        parameters.zip = zipCode;
+    } else{
+        parameters.q = query;
+    }
 
-const get = (query) =>{    
-    try {            
-        const parameters = {
-            appid: keys.APIKEY,
-            units: 'metric'
-        };
-
-        let zipCode = parseInt(query);
-        if(!isNaN(zipCode)){
-            parameters.zip = zipCode;
-        } else{
-            parameters.q = query;
-        }
-        
-        const req = https.get(`https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}`, res=>{  
+    try {
+        const req = https.get(`https://api.openweathermap.org/data/2.5/weather?${querystring.stringify(parameters)}`, res =>{                            
             if(res.statusCode === 200){
                 let body = "";
-                
                 res.on('data', data=>{
-                    body += data.toString();
-                });
+                    body += data;
+                })
 
-                res.on('end', ()=>{
-                    let result = JSON.parse(body);
-                    printMess.print(result);                       
-                });
-            }else{
-                let error = `Cannot get weather status for ${query}. Error ${res.statusCode} - ${http.STATUS_CODES[res.statusCode]}.`
-                printError.print(error);
-            }     
+                res.on('end', ()=>{                
+                    let weatherObj = JSON.parse(body);                
+                    printMess.print(weatherObj);                
+                })            
+            } else{
+                let errMess = `Cannot get weather status for ${query} (${res.statusCode} - ${http.STATUS_CODES[res.statusCode]}).`;
+                printError.print(errMess);
+            }
         })
-    
-        req.on('error', error=>{
-            printError.print(error);
-        });           
-    } catch (error) {
-        printError.print(error);
+
+        req.on('error', err=>{
+            printError.print(err);
+        })
+    } catch (err) {
+        printError.print(err);
     }
 }
 
